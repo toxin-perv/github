004ef6d897cd9b08dfc18e84a605ff2a4ec09283
toxin-perv <jangidranjan902@gmail.com>
2025-01-02 11:19:22 +0530

    print the word

---
+++ django/world/furniture/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def stock(request):
+    return HttpResponse("Stock Page")
+
+def blog(request):
+    return HttpResponse("Blog Page")
